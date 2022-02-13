aa9d94c8e07825e4844b6dc583bc40d1700ec54a
salmanpary <salmanpary@gmail.com>
2022-02-13 13:10:52 +0530

    middle ware added

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -2,7 +2,7 @@ const jwt=require('jsonwebtoken')
 const User=require('../models/user')
 const auth=async(req,res,next)=>{
     try{
-        const token=req.header('Authorization').replace('Bearer','')
+        const token=req.header('Authorization').replace('Bearer ','')
         const decoded=jwt.verify(token,'this course')
         const user=await User.findOne({_id:decoded._id,'tokens.token':token})
         if(!user){
--- src/routers/user.js
+++ src/routers/user.js
@@ -3,7 +3,7 @@ const express=require('express')
 const User=require('../models/user')
 const auth=require('../middleware/auth')
 const router=new express.Router()
-router.post("/users", async(req, res) => {
+router.post("/users/me", async(req, res) => {
     const user = new User(req.body);
     try{
       await user.save()
@@ -37,18 +37,8 @@ router.post('/users/login',async(req,res)=>{
 
   }
 })
-  router.get('/users',auth,async(req,res)=>{
-    try{
-      const users=await User.find({})
-      res.send(users)
-    }catch(e){
-      res.status(500).send()
-    }
-      // User.find({}).then((users)=>{
-      //     res.status(501).send(users)
-      // }).catch((e)=>{
-      //     res.status(500).send()
-      // })
+  router.get('/users/me',auth,auth,async(req,res)=>{
+    res.send(req.user)
   })
   router.get('/users/:id',async(req,res)=>{
       const _id=req.params.id
